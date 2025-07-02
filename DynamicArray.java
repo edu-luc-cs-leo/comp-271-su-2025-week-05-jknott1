5e0f65766b2996237ba23483b20705c35882d334
jknott1 <jknott@luc.edu>
2025-07-02 21:51:34 +0000

    implemented DynamicArray and finished midterm_review.

--- DynamicArray.java
+++ DynamicArray.java
@@ -57,39 +57,137 @@ public class DynamicArray {
     } // method add
 
     /**
-     * method to find the position of an element in the underlying array
+     * method to find the position of an element in the underlying array. ASSIGNMENT 04 NOTE: only finds the first instance of
+     * the element in the array.
      * 
      * @return -1 if string not present, otherwise underlying array position of
      *         first occurrence of string.
      */
     public int indexOf(String string) {
-        return -1234;
+        // default assignment assumes the value is not in the list, returning -1.
+        int index = -1;
+        // cursor index - starts at the first element, [0]
+        int cursor = 0;
+        // scan the underlying list to see if desired string is in the list.
+        // if i exceeds the length of the list or i is found, stop iterating through the list.
+        while (cursor < this.underlying.length && this.underlying[cursor] != string) {
+            // moves on in the list by increasing cursor index by 1.
+            cursor++;
+        }
+        // if the loop doesn't find the element, the index will be the size of the underlying array. The index also
+        // cannot be greater than the length of the array, as shown above.
+        // So, cursor can only be equal to or less than the length. If it is not equal to the length of the array,
+        // then the loop found the element
+        if (cursor != this.underlying.length) {
+            index = cursor;
+        }
+        return index;
     } // method indexOf
 
     /** Method to tell if a string exists in the underlying array */
     public boolean contains(String string) {
-        return false;
-    }
+        return (this.indexOf(string) >= 0); // uses indexOf and sees if it returns the default value or not and returns that boolean
+    } // method contains
 
     /** Method to count how many times a string appears in the underlying array*/
     public int countOf(String string) {
-        return -1234;
-    }
+        // counts how many times the instance of string appears
+        int count = 0;
+        // tracks the current index
+        int index = 0;
+        // loop iterates through the list and adds one to "count" when an instance is detected.
+        while (index < this.underlying.length) {
+            if (this.underlying[index] == string) {
+                count++;
+            }
+            // moves through underlying list
+            index++;
+        }
+        return count; //either returns 0 as the default or the number of instances.
+    } method countOf
 
     /** method to remove items from the underlying array */
     public String remove(int index) {
-        return "James on the street talking trash about your hotel";
-    }
+        // string to be returned
+        // has a default value of "null"
+        String retStr = null;
+        // initializes a new temporary array 
+        // has length of one less than the old array, as one element is being removed.
+        String[] temp  = new String[this.underlying.length - 1];
+        // if the index is valid (not negative and not greater than the array)
+        // starts a loop
+        if (!(index >= this.underlying.length || index < 0)) {
+            // return string returns the removes element
+            retStr = this.underlying[index];
+            // two loops:
+            // the first assigns the elements directly from the original array to the new one until the element at index
+            // is reached
+            // the second assigns the element starting one ahead from the index in the old array to the index in the temp array.
+            for (int i = 0; i < index ; i++) {
+                temp[i] = this.underlying[i];
+            }
+            for (int i = index; i < temp.length; i++) {
+                temp[i] = this.underlying[i + 1];
+            }
+            // assigns the temp array as the new underlying
+            this.underlying = temp;
+        }
+        return retStr;
+    } method remove
 
     /** overload remove */ 
     public String remove(String string) {
-        return "James on the street talking trash about your hotel";
-    }
+        // assigns index to the first element that matches the "string" parameter.
+        int index = this.indexOf(string);
+        // string to be returned
+        // has a default value of "null"
+        String retStr = null;
+        // initializes a new temporary array 
+        // has length of one less than the old array, as one element is being removed.
+        String[] temp  = new String[this.underlying.length - 1];
+        // if the index is valid (not negative and not greater than the array)
+        // starts a loop
+        if (!(index >= this.underlying.length || index < 0)) {
+            // return string returns the removes element
+            retStr = this.underlying[index];
+            // two loops:
+            // the first assigns the elements directly from the original array to the new one until the element at index
+            // is reached
+            // the second assigns the element starting one ahead from the index in the old array to the index in the temp array.
+            for (int i = 0; i < index ; i++) {
+                temp[i] = this.underlying[i];
+            }
+            for (int i = index; i < temp.length; i++) {
+                temp[i] = this.underlying[i + 1];
+            }
+            // assigns the temp array as the new underlying
+            this.underlying = temp;
+        }
+        return retStr;
+    } method remove
 
     /** Complete this method */
     public String toString() {
-        return "to be done shortly"; // blatant violation of magic values clause
-                                     // serves as reminder to finish this method
-    }
+        // initializing return string
+        String retStr;
+        // beginning of the string is formatted to be brackets (as arrays usually start with)
+        String begString = "[";
+        // ends with a closing brackets
+        String endString = "]";
+        // initializing the mid contents of the string, which is all the new values
+        String midString = "";
+        for (int i = 0; i < this.underlying.length; i++) {
+            // fencepost - first element is formatted differently. doesn't start with a comma and a space
+            // other than that, every element is seperated by commas and is encased in quotes
+            if (i == 0) {
+                midString += "\"" + this.underlying[i] + "\"";
+            } else {
+                midString += ", \"" + this.underlying[i] + "\"";
+            }
+        }
+        // builds the entire return string
+        retStr = begString + midString + endString;
+        return retStr;
+    } method toString
 
 } // class DynamicArray
---
+++ midterm_review.md
@@ -0,0 +1,14 @@
+# Midterm Review - James Knott
+
+If I'm going to be honest, I'm not nor have I ever really been confident in my skills in computer programming. Seeing as this
+(what I consider to be) only my third real computer science class, and second object oriented class at that, I wasn't sure how well
+I was going to do. Although I definitely have made a couple mistakes, both in coding and professionally, I think that I am doing a 
+lot better than I thought I was going to.
+
+In terms of assignments, I think I've been pretty diligent and consistent with how I code. However, there were a few issues still, as I said. First, in the beginning, I kind of had a harder time getting into the swing of things and following proper convention. There were a couple violations of the programmers pact in my earlier work - multiple return statements and magic values. I've worked on (and for the most part fixed) my issue with multiple return statements, but sometimes I get somewhat confused when elimating magic values for other non-numeric primitives. This will be my focus for the second part of the course.
+
+Last but not least, I definitely have had one professional issue: I've missed a couple (2) days of class. Both of these times, it was because I overslept. There is no complex solution to this; I need to just work on time management. Other than that I think that I definitely engage in class as much as I can, strive for timeliness, put in the work, and ask questions when I have them. One thing that I think that I con work on is engaging in supplementary material more - like extra problems from the book.
+
+All in all, as I've said earlier, I think that I'm doing fairly well in the course (better than I thought), which has given me a little more confidence in my skills.
+
+
